52fe8d1c3d20f019e9f3567d675e2b9c754283c5
barbari10 <a.elbarbare@gmail.com>
2025-02-23 21:30:36 +0200

    excercise

---
+++ src/test/java/testPackage/Tasks.java
@@ -0,0 +1,17 @@
+package testPackage;
+
+public class Tasks {
+//    Task 1
+//        driver.navigate().to("https://duckduckgo.com/");
+//        String actualTitle;
+//        actualTitle = driver.getTitle();
+//        String expectedTitle;
+//        expectedTitle = "Google";
+//        Assert.assertEquals(actualTitle, expectedTitle, "Title is not matching");
+
+//        Assert(driver.findElement(B).isDisplayed());
+
+    //driver.close();
+
+
+}
